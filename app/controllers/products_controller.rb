1948fa4b0f2a5566ac7284cf34d014df9d2c6962
Jaqueline Camarda <jaquelineassisoliveira@gmail.com>
2019-10-22 21:11:57 -0300

    Create product and routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,25 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to home_path
+    else
+      puts 'Deu ruim'
+    end
+  end
+
+  def product_params
+    params.require(:product)
+          .permit(
+            :description,
+            :color,
+            :quantity,
+            :unit_cost,
+            :product_cost,
+            :price
+          )
   end
 end
