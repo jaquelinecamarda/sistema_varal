a1b61ea86fa04e28241d9f8ba4c1efc0af9efde4
Jaqueline Camarda <jaquelineassisoliveira@gmail.com>
2019-10-24 21:11:10 -0300

    adds create product validation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,9 +6,10 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to home_path
+      redirect_to home_path # se der certo o usuário e redirecionado p/ pagina principal
     else
-      puts 'Deu ruim'
+      flash[:error] = @product.errors.full_messages.first
+      redirect_to products_new_path # se der errado ele permanece na propria página
     end
   end
 
