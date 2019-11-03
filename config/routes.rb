ddaf9959e6dfe13f580e8d0701e13244918da734
Jaqueline Camarda <jaquelineassisoliveira@gmail.com>
2019-11-03 10:34:52 -0200

    Delete product

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/products/new', to: 'products#new'
   get '/products', to: 'products#index' # nome após # é referente a action criada no controller.
+  delete '/products/:id', to: 'products#delete'
   post '/products', to: 'products#create'
 end
